f7f2d6103dc911c038dd837c27550dd91a225991
Lavia <samy.langlois@gmail.com>
2020-12-12 21:39:05 +0100

    creating svg msg box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MsgBoxComponent } from './msg-box/msg-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MsgBoxComponent
   ],
   imports: [
     BrowserModule,
