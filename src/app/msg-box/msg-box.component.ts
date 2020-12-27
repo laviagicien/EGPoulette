20a1b359459d8e19db01545ddbad720b76a3af99
Lavia <samy.langlois@gmail.com>
2020-12-27 13:32:39 +0100

    adding audio and starting to create function for pop up mesage

--- src/app/msg-box/msg-box.component.ts
+++ src/app/msg-box/msg-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Howl, Howler } from 'howler';
 
 @Component({
   selector: 'app-msg-box',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MsgBoxComponent implements OnInit {
 
+  delay = async (ms: number) => new Promise(res => setTimeout(res, ms));
+  popUpSound = new Howl({
+    src: ['../../assets/audio/popUpSound.wav'],
+    volume: 1.0
+  });
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.showMessage();
   }
 
+  async showMessage() {
+    const msgContainer = <HTMLElement>document.getElementById('msgContainer');
+    this.popUpSound.play();
+    await this.delay(1000);
+    msgContainer.classList.toggle('reveal');
+    await this.delay(60000);
+    msgContainer.classList.toggle('reveal');
+  }
 }
