b66f8ee1e131fc9f36b34b207f05e432310a6cca
Samy Langlois <samy.langlois@gmail.com>
2020-12-31 16:42:35 +0100

    slowly improving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io'
 const config: SocketIoConfig = { 
   url: 'http://localhost:3000', 
   options: {
+    extraHeaders: {
+      Authorization: "#"
+    },
     transports: ['websocket']
   } 
 };
